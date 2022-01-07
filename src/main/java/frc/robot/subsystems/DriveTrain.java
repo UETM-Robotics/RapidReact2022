1d29d65347b91991fdda7b04d56eda8a92d4dc9f
NotAMouse <61439506+NotAMouse@users.noreply.github.com>
2022-01-07 14:35:13 -0600

    added diff Drive

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -17,6 +18,8 @@ public class DriveTrain extends SubsystemBase {
 
     private final SpeedControllerGroup lDriveSide = new SpeedControllerGroup(fl, hl);
     private final SpeedControllerGroup rDriveSide = new SpeedControllerGroup(fr, hr);
+
+    private final DifferentialDrive drive = new DifferentialDrive(lDriveSide, rDriveSide);
 
     public DriveTrain() {
 
