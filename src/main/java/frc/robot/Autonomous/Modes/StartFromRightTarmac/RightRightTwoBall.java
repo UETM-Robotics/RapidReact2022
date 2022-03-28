88f0ef54d3dc65b52d11a6fce12bc108d9a1b81c
NotAMouse <61439506+NotAMouse@users.noreply.github.com>
2022-03-27 19:40:56 -0500

    Edited to respond to changes in PathPlannerLib, more streamlined and less likely to break

--- src/main/java/frc/robot/Autonomous/Modes/StartFromRightTarmac/RightRightTwoBall.java
+++ src/main/java/frc/robot/Autonomous/Modes/StartFromRightTarmac/RightRightTwoBall.java
@@ -1,7 +1,6 @@
 package frc.robot.Autonomous.Modes.StartFromRightTarmac;
 
 
-import com.pathplanner.lib.PathPlanner;
 
 import frc.robot.Actions.AimAtHub;
 import frc.robot.Actions.DriveTrajectoryAction;
@@ -10,6 +9,7 @@ import frc.robot.Autonomous.Framework.AutoModeEndedException;
 import frc.robot.Loops.RobotStateEstimator;
 import frc.robot.Utilities.Geometry.Pose2d;
 import frc.robot.Utilities.Geometry.Rotation2d;
+import frc.robot.Utilities.PathPlanner.PathPlanner;
 import frc.robot.Utilities.RamseteTrajectory.Trajectory;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Intake;
@@ -18,7 +18,8 @@ import frc.robot.subsystems.Intake.IntakeControlMode;
 
 public class RightRightTwoBall extends AutoModeBase {
 
-    Trajectory trajectory = Trajectory.fromWpiLibTrajectory(PathPlanner.loadPath("RightRightTwoBall", 4, 4));
+    //Trajectory trajectory = Trajectory.fromWpiLibTrajectory(PathPlanner.loadPath("RightRightTwoBall", 4, 4));
+    Trajectory trajectory = PathPlanner.loadPath("RightRightTwoBall", 4.0, 4.0);
 
     @Override
     protected void routine() throws AutoModeEndedException {
